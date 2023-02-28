b098d948df5a789b6a8c6cff073afdb04703a67a
homew <homew@DESKTOP-NULQPNB>
2023-02-28 19:17:18 +0300

    Добавьте файлы проекта.

---
+++ Задача 161/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.Json;
+using Задача_161;
+
+string jsonString = String.Empty;
+using (StreamReader sr = new StreamReader("Products.json"))
+{
+    jsonString = sr.ReadToEnd();
+}
+Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+Product maxProduct = products[0];
+foreach (Product e in products)
+{
+    if (e.Summa > maxProduct.Summa)
+    {
+        maxProduct = e;
+    }
+}
+Console.WriteLine($"{maxProduct.Code} {maxProduct.Name} {maxProduct.Summa}");
